58a7d12fdeb7d95181b045894e19ec3562e50169
elinekupers <elinekupers@gmail.com>
2019-11-07 12:22:38 -0500

    update script with all different plots

--- scripts/s_plotDensityRatioConesRGCV1.m
+++ scripts/s_plotDensityRatioConesRGCV1.m
@@ -1,23 +1,25 @@
 % s_plotDensityRatioConesRGCV1
 
-%% Define params
+cd(pfRV1rootPath)
+
+
+%% -------- CONES from ISETBIO ----------
 
 % Eccentricity range
-eccDeg = linspace(0,30,73); % deg
+% eccDeg = linspace(0,30,73); % deg
 
 % Polar angle range
-angDeg = (0:5:360);  % deg
+% angDeg = (0:5:360);  % deg
 % angDeg = deg2rad([0, 90, 180, 270, 360]);
 
-%% -------- CONES ISETBIO ----------
 % plotConeDensityIsetbio(angDeg, eccDeg)
 
-%% -------- RGC ISETBIO (Not yet implemented) ----------
+%% -------- RGC from ISETBIO (Not yet implemented) ----------
 % fov = 2; % deg
 % plotConeDensityIsetbio(fov)
 
 
-%% ---------- CONES, RGC from displacement map toolbox ---------
+%% ---------- CONES and RGC from displacement map toolbox ---------
 
 sampleResEccen      = 1; % deg
 sampleResPolAng     = 5; % deg
@@ -25,3 +27,16 @@ maxEccen            = 20; % deg
 pixelsPerDegVisual  = 10; % resolution for plotting (more pixels=higher resolution)
 
 allMaps = plotConeAndRGCDensityDisplacementMap(sampleResEccen, sampleResPolAng, maxEccen, pixelsPerDegVisual);
+
+
+
+%% ---------- CONES from Noah's SFN poster ---------
+conesSong = plotConesSong;
+
+
+%% ---------- RGC from Noah's SFN poster ---------
+rgcWatson = plotmRGCRFWatson;
+
+
+%% ---------- V1 CMF from Noah's SFN poster ---------
+v1CMF = plotV1CMFHCP;
