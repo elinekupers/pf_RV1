667c21df4e06a68d21fa0fbbdf551dffc1c7a8b8
elinekupers <elinekupers@gmail.com>
2019-11-20 18:24:13 -0500

    add CMF function described by Horton and Hoyt in 1991

---
+++ scripts/subfunctions/HortonHoytCMF.m
@@ -0,0 +1,11 @@
+function CMF = HortonHoytCMF(eccentricity)
+% CMF as a function of eccentricity, from Horton & Hoyt (1991). The
+% representation of the visual field in human striate cortex. A revision of
+% the classic Holmes map. Arch Ophthalmol. 1991 Jun;109(6):816-24.
+
+% CMF in mm2/deg2, eccentricity in deg
+
+    CMF_mmperdeg = 17.3./(eccentricity+0.75);
+    CMF = CMF_mmperdeg.^2;
+
+end
