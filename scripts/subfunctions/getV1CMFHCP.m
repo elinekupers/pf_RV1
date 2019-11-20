8db7640ef9980c759dc624fa19a57c842a164033
elinekupers <elinekupers@gmail.com>
2019-11-20 18:28:03 -0500

    get individual data so that average can be done after computing vma and vma, change v1v and v1d labels so that meridian data are in retinal coordinates--guess we should allow to plot data in both retinal or visual but i guess we are sticking to retinal coords for now

--- scripts/subfunctions/getV1CMFHCP.m
+++ scripts/subfunctions/getV1CMFHCP.m
@@ -22,8 +22,8 @@ superiorIdx = temporalIdx;
 for ii = 1:size(T,1)
     temporalIdx(ii) = (strcmp(T.meridian{ii}, 'horizontal') && strcmp(T.label{ii},'V1d'));
     nasalIdx(ii)    = (strcmp(T.meridian{ii}, 'horizontal') && strcmp(T.label{ii},'V1v'));
-    inferiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1d'));
-    superiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1v'));
+    inferiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1v')); % ventral = upper visual field, thus corresponding to inferior retina
+    superiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1d')); % dorsal = lower visual field, thus corresponding to superior retina
 end
 
 % Get integral of V1 CMF for three different eccentricity wedges
@@ -44,8 +44,8 @@ for jj = 1:3
     temporalMeanSubject = mean(temporal);
     inferiorMeanSubject = mean(inferior);
 
-    dataMeridians.(fn{jj}) = [nasalMeanSubject, superiorMeanSubject, temporalMeanSubject, inferiorMeanSubject];
-
+    dataMeridians.meanSubject.(fn{jj}) = [nasalMeanSubject, superiorMeanSubject, temporalMeanSubject, inferiorMeanSubject];
+    dataMeridians.individualSubjects.(fn{jj})   = cat(3, [nasal, superior, temporal, inferior]);
 % fprintf('V1 CMF 1-6 deg eccen, 15 deg polar angle wedge')
 % fprintf('Horizontal-Vertical Asymmetry (Horz / Vert):\t %1.2f%%\n', hva(dataIn))
 % fprintf('Vertical-Meridian Asymmetry (North / South):\t %1.2f%%\n', vma(dataIn))
