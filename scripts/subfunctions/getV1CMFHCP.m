193810e6711bf445f5944a33d6a6c96463c0a2f8
elinekupers <elinekupers@gmail.com>
2019-11-29 19:17:18 -0500

    use new table with more eccen

--- scripts/subfunctions/getV1CMFHCP.m
+++ scripts/subfunctions/getV1CMFHCP.m
@@ -4,51 +4,53 @@ function dataMeridians = getV1CMFHCP()
 % on Noah Benson's SFN 2019 poster.
 
 %% V1 CMF
-T = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'HV-DV-surface-areas.csv'));
+% T1 = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'distance-ROI-data_angle-stepsize=10.csv'));
+T1 = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'distance-ROI-data_angle-stepsize=10_full.csv'));
 
-allMasks.eccen1_6   = T.min_eccen==1; % deg
-allMasks.eccen0_35  = T.min_eccen==0; % deg
-allMasks.eccen35_7  = T.min_eccen==3.5; % deg
-polang              = T.delta_angle==30; % deg
 
-cmfData = T.surface_area_mm2;
+allMasks.eccen1_6   = (T1.min_eccentricity_deg==1 & T1.max_eccentricity_deg==6); % deg
+allMasks.eccen0_35  = T1.min_eccentricity_deg==0; % deg
+allMasks.eccen35_7  = T1.min_eccentricity_deg==3.5; % deg
+allMasks.eccen05_1  = T1.min_eccentricity_deg==0.5; % deg
+allMasks.eccen1_2   = (T1.min_eccentricity_deg==1 & T1.max_eccentricity_deg==2); % deg
+allMasks.eccen2_4   = (T1.min_eccentricity_deg==2 & T1.max_eccentricity_deg==4); % deg
+allMasks.eccen4_6   = (T1.min_eccentricity_deg==4 & T1.max_eccentricity_deg==6); % deg
+allMasks.eccen4_8   = (T1.min_eccentricity_deg==4 & T1.max_eccentricity_deg==8); % deg
+allMasks.eccen6_8   = (T1.min_eccentricity_deg==6 & T1.max_eccentricity_deg==8); % deg
+
+
+polang              = T1.angle_delta_deg==30; % deg
+
+cmfData = T1.surface_area_mm2;
 
 % TO CHECK
-temporalIdx = zeros(size(T,1),1);
-nasalIdx    = temporalIdx;
-inferiorIdx = temporalIdx;
-superiorIdx = temporalIdx;
+horizontalVF = zeros(size(T1,1),1);
+verticalVF   = horizontalVF;
+upperVF      = horizontalVF;
+lowerVF      = horizontalVF;
 
-for ii = 1:size(T,1)
-    temporalIdx(ii) = (strcmp(T.meridian{ii}, 'horizontal') && strcmp(T.label{ii},'V1d'));
-    nasalIdx(ii)    = (strcmp(T.meridian{ii}, 'horizontal') && strcmp(T.label{ii},'V1v'));
-    inferiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1v')); % ventral = upper visual field, thus corresponding to inferior retina
-    superiorIdx(ii) = (strcmp(T.meridian{ii}, 'vertical') && strcmp(T.label{ii},'V1d')); % dorsal = lower visual field, thus corresponding to superior retina
+for ii = 1:size(T1,1)
+    horizontalVF(ii) = (strcmp(T1.boundary{ii}, 'horizontal'));
+    verticalVF(ii)   = (strcmp(T1.boundary{ii}, 'vertical'));
+    upperVF(ii)      = (strcmp(T1.boundary{ii}, 'ventral')); % ventral = upper visual field, corresponding to inferior retina
+    lowerVF(ii)      = (strcmp(T1.boundary{ii}, 'dorsal')); % dorsal = lower visual field,  corresponding to superior retina
 end
 
 % Get integral of V1 CMF for three different eccentricity wedges
 fn = fieldnames(allMasks);
 
 dataMeridians = struct();
-for jj = 1:3
+
+for jj = 1:numel(fn)
     
     thisEccenMask = allMasks.(fn{jj});
     
-    nasal    = cmfData(nasalIdx & thisEccenMask & polang);
-    superior = cmfData(superiorIdx & thisEccenMask & polang);
-    temporal = cmfData(temporalIdx & thisEccenMask & polang);
-    inferior = cmfData(inferiorIdx & thisEccenMask & polang);
+    horz    = cmfData(horizontalVF & thisEccenMask & polang);
+    vert = cmfData(verticalVF & thisEccenMask & polang);
+    upr = cmfData(upperVF & thisEccenMask & polang);
+    lowr = cmfData(lowerVF & thisEccenMask & polang);
        
-    nasalMeanSubject = mean(nasal);
-    superiorMeanSubject = mean(superior);
-    temporalMeanSubject = mean(temporal);
-    inferiorMeanSubject = mean(inferior);
-
-    dataMeridians.meanSubject.(fn{jj}) = [nasalMeanSubject, superiorMeanSubject, temporalMeanSubject, inferiorMeanSubject];
-    dataMeridians.individualSubjects.(fn{jj})   = cat(3, [nasal, superior, temporal, inferior]);
-% fprintf('V1 CMF 1-6 deg eccen, 15 deg polar angle wedge')
-% fprintf('Horizontal-Vertical Asymmetry (Horz / Vert):\t %1.2f%%\n', hva(dataIn))
-% fprintf('Vertical-Meridian Asymmetry (North / South):\t %1.2f%%\n', vma(dataIn))
+    dataMeridians.individualSubjects.(fn{jj})   = cat(3, [horz, vert, upr, lowr]);
 
 end
 
