7b631f96c51724bcc206bf8f21a27cdee03be20b
elinekupers <elinekupers@gmail.com>
2020-03-13 10:46:40 -0400

    cope with different input array sizes, add dummy dimensions if needed

--- scripts/subfunctions/classifyBradleyRGCResponse.m
+++ scripts/subfunctions/classifyBradleyRGCResponse.m
@@ -1,28 +1,25 @@
-function P = classifyBradleyRGCResponse(T_pool_stim, saveDir, saveData)
+function P = classifyBradleyRGCResponse(T_pool_stim)
 
-[rows, cols, tLoc, tOrient, tEccen, nTrials] =size(T_pool_stim);
+sz=size(T_pool_stim);
+dataIn = [];
+if length(sz)==4 % [rows, cols, tOrient, nTrials]
+    
+    % add singleton dimension for time points
+    data(1,:,:,:,:) = T_pool_stim; % we have time samples, rows, cols, stim orientations, nTrials
+    data = permute(data, [5, 2, 3, 1, 4]); % we want nTrials, rows, cols, time samples, stim orientations
+    
+elseif length(sz)==3 % [rows, cols, tOrient]
+    % add singleton dimension for time points
+    
+    data(1,1,:,:,:) = T_pool_stim; % we have n trials, time samples, rows, cols, stim orientations
+    data = permute(data, [1, 3, 4, 2, 5]); % we want n trials, rows, cols, time samples, stim orientations
+else
+    error('(%s) input data has too many dimensions!', mfilename)
+end
 
-% we need dims: trials x rows x cols x time samples x stimuli) 
-data = permute(T_pool_stim, [6, 1, 2, 3, 5, 4]);
-
-for te = 1:tEccen
-    
-    tmpData = squeeze(data(:,:,:,:,te,:));
-    
-    for tl = 1:tLoc
-        
-        dataIn = tmpData(:,:,:,tl,:);
-        
-        % Save percent correct
-        P(te,tl) = getClassifierAccuracy(dataIn);
-        
-    end
-    
-    if saveData
-        if ~exist(fullfile(saveDir, 'classification', 'rgcBradley'), 'dir')
-            mkdir(fullfile(saveDir, 'classification', 'rgcBradley'));
-        end
-        save(fullfile(saveDir, 'classification', 'rgcBradley', 'classify_rgcResponse_Bradley.mat'), 'P', '-v7.3')
-    end
+    % Save percent correct
+    P = getClassifierAccuracy(data);
     
 end
+
+
