00d3d1fff3e284d8bc2c04ab1f2c7e13819852d7
elinekupers <elinekupers@gmail.com>
2020-03-13 10:20:32 -0400

    draw stim frequency as a circle in plot and plot normalized amps

--- scripts/subfunctions/plotFFTDoG.m
+++ scripts/subfunctions/plotFFTDoG.m
@@ -1,38 +1,39 @@
 function [] = plotFFTDoG(DoGfilter,  rgcParams, expParams, saveFigs)
 
-
-
 fftAmpsDoG = abs(fft2(DoGfilter));
 fftAmpsDoG_normalized = fftAmpsDoG./max(fftAmpsDoG(:))';
 
 midpoint = ceil(size(fftAmpsDoG,1)/2);
 quarterpoint = midpoint/2;
-sfOfStim = expParams.spatFreq*2;
+
+% get stim sf and get coords to draw circle
+sfOfStim = expParams.spatFreq * rgcParams.fov;
+
+th = 0:pi/50:2*pi;
+xvals = sfOfStim * cos(th) + midpoint;
+yvals = sfOfStim * sin(th) + midpoint;
+
 
 % Visualize fft amps
 fH = figure(1); clf;
-imagesc(fftshift(fftAmpsDoG)); hold all;
+imagesc(fftshift(fftAmpsDoG_normalized)); hold all;
 
-% center cross
+% draw center cross
 plot([midpoint, midpoint], [1,size(fftAmpsDoG,1)], 'w:');
 plot([1,size(fftAmpsDoG,1)],[midpoint, midpoint],  'w:');
 
-% stim sf cross
-plot([midpoint-sfOfStim, midpoint-sfOfStim], [1,size(fftAmpsDoG,1)], 'r:', 'lineWidth',3);
-plot([1,size(fftAmpsDoG,1)],[midpoint-sfOfStim, midpoint-sfOfStim],  'r:', 'lineWidth',3);
+% draw stim sf cross
+plot(xvals, yvals, 'r:', 'lineWidth',3);
 
-plot([midpoint+sfOfStim, midpoint+sfOfStim], [1,size(fftAmpsDoG,1)], 'r:', 'lineWidth',3);
-plot([1,size(fftAmpsDoG,1)],[midpoint+sfOfStim, midpoint+sfOfStim],  'r:', 'lineWidth',3);
-
+% Make figure pretty
 colormap gray; colorbar; axis square;
-
 title(sprintf('Normalized FFT amps DoG filter %d',rgcParams.cone2RGCRatio), 'FontSize',17);
-set(gca,'CLim', [0 max(fftAmpsDoG(:))], ...
+set(gca,'CLim', [0 max(fftAmpsDoG_normalized(:))], ...
     'FontSize', 17, 'TickDir', 'out', ...
     'XTick', [midpoint*0.5, midpoint, midpoint*1.5], ...
-    'XTickLabel',{num2str(-quarterpoint), '0', num2str(quarterpoint)}, ...
+    'XTickLabel',{num2str(-quarterpoint/rgcParams.fov), '0', num2str(quarterpoint/rgcParams.fov)}, ...
     'YTick', [midpoint*0.5, midpoint, midpoint*1.5], ...
-    'YTickLabel',{num2str(-quarterpoint), '0', num2str(quarterpoint)});
+    'YTickLabel',{num2str(-quarterpoint/rgcParams.fov), '0', num2str(quarterpoint/rgcParams.fov)});
 xlabel('cycles/deg'); box off;
 
 
