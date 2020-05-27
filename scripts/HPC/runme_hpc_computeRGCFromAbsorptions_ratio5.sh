b5150a910007c20ba3e9940d59daa8c0bbd46fa8
elinekupers <ek99@nyu.edu>
2020-05-27 16:36:29 -0400

    lower memory of node

--- scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio5.sh
+++ scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio5.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time 20:00:00 # time (D-HH:MM)
-#SBATCH --mem=250GB # memory pool for all cores
+#SBATCH --mem=125GB # memory pool for all cores
 #SBATCH --job-name=linearRGCModel_Ratio5
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ek99@nyu.edu
