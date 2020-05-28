834d60536ad90a477873c505c3f34e9a210670c0
elinekupers <ek99@nyu.edu>
2020-05-28 12:26:05 -0400

    fix prep function name

--- scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio2.sh
+++ scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio2.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time 20:00:00 # time (D-HH:MM)
-#SBATCH --mem=250GB # memory pool for all cores
-#SBATCH --job-name=linearRGCModel_Ratio2
+#SBATCH --mem=125GB # memory pool for all cores
+#SBATCH --job-name=Ratio2_linearRGCModel
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ek99@nyu.edu
 #SBATCH -o slurm.%N.%j.out
@@ -17,7 +17,7 @@ cd /scratch/ek99/pf_RV1/
 # If the files you are running are not in the same folder as this script,
 # you can insert "addpath(genpath('/PATH/TO/FILES/'));" before the command
 # you want to run.
-matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = prepHPC($SLURM_ARRAY_TASK_ID); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 2); exit()"
+matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = pfRV1_prepHPC($SLURM_ARRAY_TASK_ID); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 2); exit()"
 
 exit
 
--- scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio4.sh
+++ scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio4.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time 20:00:00 # time (D-HH:MM)
 #SBATCH --mem=125GB # memory pool for all cores
-#SBATCH --job-name=linearRGCModel_Ratio4
+#SBATCH --job-name=Ratio4_linearRGCModel
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ek99@nyu.edu
 #SBATCH -o slurm.%N.%j.out
@@ -17,7 +17,7 @@ cd /scratch/ek99/pf_RV1/
 # If the files you are running are not in the same folder as this script,
 # you can insert "addpath(genpath('/PATH/TO/FILES/'));" before the command
 # you want to run.
-matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = prepHPC($SLURM_ARRAY_TASK_ID); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 4); exit()"
+matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = pfRV1_prepHPC($SLURM_ARRAY_TASK_ID); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 4); exit()"
 
 exit
 
