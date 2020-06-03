19e4df668647122cb2d0c0b01ec6e43e8915aff6
elinekupers <elinekupers@gmail.com>
2020-06-03 16:25:55 -0400

    update hpc bash scripts for ratio 5

--- scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio5_run4.sh
+++ scripts/HPC/runme_hpc_computeRGCFromAbsorptions_ratio5_run4.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time 20:00:00 # time (D-HH:MM)
 #SBATCH --mem=125GB # memory pool for all cores
-#SBATCH --job-name=Ratio5_run1_linearRGCModel
+#SBATCH --job-name=Ratio5_run4_linearRGCModel
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ek99@nyu.edu
 #SBATCH -o slurm.%N.%j.out
@@ -17,7 +17,7 @@ cd /scratch/ek99/pf_RV1/
 # If the files you are running are not in the same folder as this script,
 # you can insert "addpath(genpath('/PATH/TO/FILES/'));" before the command
 # you want to run.
-matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = pfRV1_prepHPC(1); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 5, $SLURM_ARRAY_TASK_ID); exit()"
+matlab -nodisplay -r "addpath(genpath('/scratch/ek99/pf_RV1')); addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); [expName, subFolder, seed] = pfRV1_prepHPC(4); linearRGCModel(pfRV1rootPath, subFolder, expName, seed, 5, $SLURM_ARRAY_TASK_ID); exit()"
 
 exit
 
