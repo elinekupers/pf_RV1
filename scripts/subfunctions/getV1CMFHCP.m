865cc93df885d4d1e2593354d7ba57efcd5dc179
elinekupers <elinekupers@gmail.com>
2020-06-03 10:30:48 -0400

    clean up and add comments

--- scripts/subfunctions/getV1CMFHCP.m
+++ scripts/subfunctions/getV1CMFHCP.m
@@ -1,67 +1,63 @@
 function dataMeridians = getV1CMFHCP()
-% Get V1 cortical magnification factor data (CMF, mm2 of cortex / deg
-% visual field) between 1 and 6 degrees eccentricity. These data were used
-% on Noah Benson's SFN 2019 poster.
+% Function to compute V1 cortical magnification factor data (CMF) in mm2
+% cortex/deg2 visual field) between 1 and 6 degrees eccentricity.
+%
+% These data come from the published article:
+%   Benson, Jamison, Arcaro, Vu,Glasser, Coalson, VanEssen, Yacoub, Ugurbil,
+%   Winawer, Kay. 2018 The Human Connectome Project 7 Tesla retinotopy
+%   dataset: Description and population receptive field analysis.
+%   Journal of Vision, 18:23. doi:10.1167/18.12.13.
+% 
+% Written by EK 2019 NYU
 
-%% V1 CMF
-% T1 = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'distance-ROI-data_angle-stepsize=10.csv'));
-% T1 = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'distance-ROI-data_angle-stepsize=10_full.csv'));
-T1 = readtable(fullfile(pfRV1rootPath, 'external', 'data', 'distance-ROI-data_angle-stepsize=5_eccen-stepsize=1.csv'));
+%% 0. Read data in
+T1 = readtable(fullfile(pfRV1rootPath, 'data', 'DROI_table.csv'));
+cmfData = T1.surface_area_mm2;
 
+numSubjects = size(T1,1);
 
-
-% allMasks.eccen1_6   = (T1.min_eccentricity_deg==1 & T1.max_eccentricity_deg==6); % deg
-% allMasks.eccen0_35  = T1.min_eccentricity_deg==0; % deg
-% allMasks.eccen35_7  = T1.min_eccentricity_deg==3.5; % deg
-% allMasks.eccen05_1  = T1.min_eccentricity_deg==0.5; % deg
-% allMasks.eccen1_2   = (T1.min_eccentricity_deg==1 & T1.max_eccentricity_deg==2); % deg
-% allMasks.eccen2_4   = (T1.min_eccentricity_deg==2 & T1.max_eccentricity_deg==4); % deg
-% allMasks.eccen4_6   = (T1.min_eccentricity_deg==4 & T1.max_eccentricity_deg==6); % deg
-% allMasks.eccen4_8   = (T1.min_eccentricity_deg==4 & T1.max_eccentricity_deg==8); % deg
-% allMasks.eccen6_8   = (T1.min_eccentricity_deg==6 & T1.max_eccentricity_deg==8); % deg
-
-allMasks.eccen0_1   = (T1.min_eccentricity_deg==0 & T1.max_eccentricity_deg==1); % deg
+%% 1. Define eccentricity, polar angle data masks
 allMasks.eccen1_2   = (T1.min_eccentricity_deg==1 & T1.max_eccentricity_deg==2); % deg
 allMasks.eccen2_3   = (T1.min_eccentricity_deg==2 & T1.max_eccentricity_deg==3); % deg
 allMasks.eccen3_4   = (T1.min_eccentricity_deg==3 & T1.max_eccentricity_deg==4); % deg
 allMasks.eccen4_5   = (T1.min_eccentricity_deg==4 & T1.max_eccentricity_deg==5); % deg
 allMasks.eccen5_6   = (T1.min_eccentricity_deg==5 & T1.max_eccentricity_deg==6); % deg
-allMasks.eccen6_7   = (T1.min_eccentricity_deg==6 & T1.max_eccentricity_deg==7); % deg
-allMasks.eccen7_8   = (T1.min_eccentricity_deg==7 & T1.max_eccentricity_deg==8); % deg
 
 polang              = T1.angle_delta_deg==10; % deg
 
-cmfData = T1.surface_area_mm2;
+%% 2. Get CMF data per polar angle
 
 % preallocate space
-horizontalVF = zeros(size(T1,1),1);
+horizontalVF = NaN(numSubjects,1);
 verticalVF   = horizontalVF;
 upperVF      = horizontalVF;
 lowerVF      = horizontalVF;
 
-for ii = 1:size(T1,1)
+% Get indices for visual fields
+for ii = 1:numSubjects
     horizontalVF(ii) = (strcmp(T1.boundary{ii}, 'horizontal'));
     verticalVF(ii)   = (strcmp(T1.boundary{ii}, 'vertical'));
     upperVF(ii)      = (strcmp(T1.boundary{ii}, 'ventral')); % ventral = upper visual field, corresponding to inferior retina
     lowerVF(ii)      = (strcmp(T1.boundary{ii}, 'dorsal')); % dorsal = lower visual field,  corresponding to superior retina
 end
 
-% Get integral of V1 CMF for three different eccentricity wedges
+% Use indices to get data for visual fields
 fn = fieldnames(allMasks);
 
+% Get data struct
 dataMeridians = struct();
 
 for jj = 1:numel(fn)
-    
+    % Select eccentricity mask
     thisEccenMask = allMasks.(fn{jj});
-    
+    % Select corresponding CMF data for visual field, eccen, polar ang
     horz    = cmfData(horizontalVF & thisEccenMask & polang);
     vert    = cmfData(verticalVF & thisEccenMask & polang);
     upr     = cmfData(upperVF & thisEccenMask & polang);
     lowr    = cmfData(lowerVF & thisEccenMask & polang);
-       
+    
+    % Concatenate data
     dataMeridians.individualSubjects.(fn{jj})   = cat(3, [horz, vert, upr, lowr]);
-
 end
 
-
+return
