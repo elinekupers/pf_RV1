61ad132e7b11a736ee668c853aa4ef62a6624e38
elinekupers <elinekupers@gmail.com>
2020-06-12 11:22:41 -0400

    clean up, remove song old data, since we dont plot it

--- scripts/subfunctions/visualizeConeDensityVsEccenIsetbio.m
+++ scripts/subfunctions/visualizeConeDensityVsEccenIsetbio.m
@@ -1,5 +1,5 @@
 function [fH1, fH2, fH3, fH4] = visualizeConeDensityVsEccenIsetbio(conesCurcioIsetbio, conesSongIsetbioYoung, ...
-                       conesSongIsetbioOld, meridianIdx, eccDeg, figureDir, saveFigures)
+                      meridianIdx, eccDeg, figureDir, saveFigures)
                    
                    
 % ------------ Visualize meridan cone density vs eccentricity ------------
@@ -16,10 +16,6 @@ titleStr = 'Cone density Song et al 2011 Group 1 - ISETBIO left eye';
 meridianData.conesSongIsetbioYoung = conesSongIsetbioYoung(meridianIdx,:);
 fH2 = plotMeridiansVsEccen(meridianData.conesSongIsetbioYoung, eccDeg, titleStr, yl, figureDir, saveFigures);
 
-titleStr = 'Cone density Song et al 2011 Group 2 - ISETBIO left eye';
-meridianData.conesSongIsetbioOld = conesSongIsetbioOld(meridianIdx,:);
-plotMeridiansVsEccen(meridianData.conesSongIsetbioOld, eccDeg, titleStr, yl, figureDir, saveFigures);
-
 
 % ------------ Plot HVA and VMA vs eccen for cones and mRGC RF ------------
 titleStr = 'cone density Curcio et al 1990 - ISETBIO left eye';
@@ -27,6 +23,3 @@ fH3 = plotHVAandVMA(meridianData.conesCurcioIsetbio,[],eccDeg, visualfieldFlag, 
 
 titleStr = 'cone density Song et al 2011 - Group 1 - ISETBIO left eye';
 fH4 = plotHVAandVMA(meridianData.conesSongIsetbioYoung,[], eccDeg,visualfieldFlag, titleStr, figureDir, saveFigures);
-
-titleStr = 'cone density Song et al 2011 Group 2 - ISETBIO left eye';
-plotHVAandVMA(meridianData.conesSongIsetbioOld,[],  eccDeg, visualfieldFlag, titleStr, figureDir, saveFigures);
--- scripts/subfunctions/visualizeRGCDensityWatson.m
+++ scripts/subfunctions/visualizeRGCDensityWatson.m
@@ -1,10 +1,8 @@
 function [fH9, fH10] = visualizeRGCDensityWatson(rgcWatson, eccDeg, fH8, figureDir, saveFigures)
 
-
 % ------ Visualize density and HVA vs VMA ------ 
 titleStr = 'mRGC RF density Watson 2014 - ISETBIO';
-fH9 = plotMeridiansVsEccen(rgcWatson, [], eccDeg, false, titleStr, [], figureDir, saveFigures);
-
+fH9 = plotMeridiansVsEccen(rgcWatson, eccDeg, titleStr, [], figureDir, saveFigures);
 
 titleStr = 'HVA VMA mRGCf density Watson 2014 - ISETBIO';
 fH10 = plotHVAandVMA(rgcWatson, [], eccDeg, false, titleStr, figureDir, saveFigures);
@@ -36,7 +34,7 @@ plot(h(1).Children(2).XData,h(1).Children(2).YData, ...
                       'VMA mRGCf Barnett & Aguirre 2018 - rgcDisplacementMap'}, 'Location', 'SouthEast');
     legend boxoff;
     title('mRGC VMA')
-    set(gca, 'YLim', [-10, 80]);
+    set(gca, 'YLim', [-20, 100]);
 
 savefig(fullfile(pfRV1rootPath, 'figures', 'HVA_VMA_mRGC_RF_density_rgcDisplacementMap_Watson'))
 print(fullfile(pfRV1rootPath, 'figures', 'HVA_VMA_mRGC_RF_density_rgcDisplacementMap_Watson'), '-dpdf', '-fillpage')
