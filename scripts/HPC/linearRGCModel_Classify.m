e8248d5e5eca3b84b5421e5aaf61636ed5e96bf8
elinekupers <elinekupers@gmail.com>
2020-12-18 12:24:53 -0500

    update header and save data folder

--- scripts/HPC/linearRGCModel_Classify.m
+++ scripts/HPC/linearRGCModel_Classify.m
@@ -1,17 +1,45 @@
 function [] = linearRGCModel_Classify(baseFolder, subFolder, expName, seed, ratio, eccen)
+%
+% Function to classify RGC responses computed by linearRGCmodel.m.
+%
+% Our classifier determines whether the responses belong to a trial with a
+% 2-AFC orientation discrimination task (was Gabor clockwise or counter-
+% clockwise oriented). This function applies a 2D FFT on the RGC responses 
+% before classifying with a linear SVM.  
+%
+% See linearRGCModel.m to get mRGC responses.
+%
+% INPUTS
+% baseFolder    :   folder where rgc data live 
+% subFolder     :   subfolder name when running multiple iterations.
+% expName       :   string with experiment name, e.g. 'conedensity' (for
+%                   all options see loadExpParams.m)
+% seed          :   random number generator seed, in manuscript, we use the
+%                   following rule: run1 has rng seed 1, run2 has 2.. etc.
+% ratio         :   integer between 1-5 to indicate cone:mRGC ratio
+%                   because one mRGC = ON + OFF cell, 1 = 2:1, 2 = 1:1, 3 = 0.67:1, 4 = 0.5:1, 5 = 0.4:1 
+% eccen         :   index of eccentricity vector, for conedensity exp, eccentricies = [0 0.5 1 2 4.5 5 10:5:40] 
+%
+% Example:
+% baseFolder = '/Volumes/server/Projects/PerformanceFields_RetinaV1Model/';
+% subFolder  = 'run1'
+% expName    = 'conedensity'
+% seed       = 1; % run1 has rng seed 1, run2 has 2.. etc.
+% ratio      = 2; % cone:mRGC ratio = 1:1 (actually 2:2, as one mRGC = ON + OFF cell) 
+% eccen      = 5; % equal to 4.5 deg eccen
 
+% linearRGCModel_Classify(baseFolder, subFolder, expName, seed, ratio, eccen)
 
+%% 0. Define params
+% set random number generator seed and general flags
 rng(seed);
 
 saveData = true;
-saveFigs = true;
 
 % Get experimental params
-% subFolder = 'onlyL_highcontrasts'; %'onlyL'
-% expName   = 'defaultnophaseshift'; %'idealobserver';
 expParams = loadExpParams(expName, false);   % (false argument is for not saving params in separate matfile)
-inputType = 'absorptionrate'; % could also be 'current'
-if strcmp(inputType, 'absorptionrate')
+inputType = 'absorptions'; % can be 'absorptions' or 'current'
+if strcmp(inputType, 'absorptions')
     contrasts = expParams.contrastLevels;
     selectTimePoints = 1:28;
 elseif strcmp(inputType, 'current')
@@ -52,10 +80,6 @@ rgcParams.seed       = seed;
 
 cone2RGCRatio = ratio;
 
-
-
-
-
 %% Compute RGC responses from linear layer
 
 if ~exist(fullfile(baseFolder, 'data',  expName, 'rgc', subFolder), 'dir')
@@ -83,13 +107,17 @@ switch expName
             
             % Get SVM classifier performance in percent correct
             P_svm(c) = getClassifierAccuracy(dataIn(:,:,:,selectTimePoints,:));
+            
             fprintf('%3.2f\n',P_svm(c))
         end
         
         % Save classification results
         if saveData
-            if ~exist(fullfile(baseFolder, 'data',  expName, 'classification','rgc', subFolder), 'dir'); mkdir(fullfile(baseFolder, 'data',  expName, 'classification','rgc', subFolder)); end
-            parsave(fullfile(baseFolder, 'data', expName, 'classification', 'rgc', subFolder, sprintf('classifySVM_rgcResponse_Cones2RGC%d_%s_%d_%s_%s.mat', cone2RGCRatio, inputType, eccen, expName, subFolder)), 'P_svm',P_svm, 'rgcParams',rgcParams, 'expParams', expParams);
+            if ~exist(fullfile(baseFolder, 'data',  expName, 'classification','rgc', subFolder), 'dir'); 
+                mkdir(fullfile(baseFolder, 'data',  expName, 'classification','rgc', subFolder)); end
+            parsave(fullfile(baseFolder, 'data', expName, 'classification', 'rgc', subFolder, ...
+                sprintf('classifySVM_rgcResponse_Cones2RGC%d_%s_%d_%s_%s.mat', cone2RGCRatio, inputType, eccen, expName, subFolder)), ...
+                'P_svm',P_svm, 'rgcParams',rgcParams, 'expParams', expParams);
         end
 end
 return
