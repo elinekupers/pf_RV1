3ae8df8138141b0e2497bb2c3a3ba467fe7836ea
elinekupers <elinekupers@gmail.com>
2020-12-27 17:19:59 -0500

    only classify relevant time points

--- scripts/HPC/linearRGCModel_Classify.m
+++ scripts/HPC/linearRGCModel_Classify.m
@@ -103,10 +103,14 @@ switch expName
         for c = 1:length(contrasts)
             
             load(fullfile(baseFolder, 'data',  expName, 'rgc', subFolder, sprintf('ratio%d',ratio), sprintf('rgcResponse_Cones2RGC%d_contrast%1.4f_eccen%2.2f_%s.mat', cone2RGCRatio,  contrasts(c), eccentricities(eccen), inputType)), 'rgcResponse');
-            dataIn = rgcResponse;
+            if size(rgcResponse,4) > length(selectTimePoints) 
+                dataIn = rgcResponse(:,:,:,selectTimePoints,:);
+            else
+                dataIn = rgcResponse;
+            end
             
             % Get SVM classifier performance in percent correct
-            P_svm(c) = getClassifierAccuracy(dataIn(:,:,:,selectTimePoints,:));
+            P_svm(c) = getClassifierAccuracy(dataIn);
             
             fprintf('%3.2f\n',P_svm(c))
         end
