7bfa3b8251254211f3fc2a5413b7be8f7c108d51
elinekupers <elinekupers@gmail.com>
2020-12-29 14:00:07 -0500

    add second input to change subfolder name

--- scripts/HPC/pfRV1_prepHPC.m
+++ scripts/HPC/pfRV1_prepHPC.m
@@ -1,6 +1,11 @@
-function [expName, subFolder, seed] = pfRV1_prepHPC(taskID)
+function [expName, subFolder, seed] = pfRV1_prepHPC(taskID,varargin)
 
 % function to run different experiments with different seeds on NYUs HPC
+if nargin<2
+    prefixSubfolder = '';
+else
+    prefixSubfolder = varargin{1};
+end
 
 
 
@@ -8,17 +13,17 @@ switch taskID
     
     case {1, 2, 3, 4, 5}
         expName   = 'conedensity';
-        subFolder = sprintf('run%d', taskID);
+        subFolder = sprintf('run%d%s', taskID,prefixSubfolder);
         seed      = taskID;
 
     case {6, 7, 8, 9, 10}
         expName   = 'defaultnophaseshift';
-        subFolder = sprintf('run%d', taskID-5);
+        subFolder = sprintf('run%d%s', taskID-5, prefixSubfolder);
         seed      = taskID-5;
         
     case {11, 12, 13, 14, 15}
         expName   = 'default';
-        subFolder = sprintf('run%d', taskID-10);
+        subFolder = sprintf('run%d%s', taskID-10, prefixSubfolder);
         seed      = taskID-10;
         
 end
