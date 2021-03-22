4b4f8b7db31af5292becd6f63acba7c3d8213c27
elinekupers <elinekupers@gmail.com>
2021-03-22 11:59:08 -0400

    update plotting for defaultnophaseshift

--- scripts/s_testUsingStimTemplateInClassifier.m
+++ scripts/s_testUsingStimTemplateInClassifier.m
@@ -11,7 +11,7 @@ baseFolder = '/Volumes/server/Projects/PerformanceFields_RetinaV1Model/';
 subFolder  = 'run1';
 expName    = 'defaultnophaseshift';
 seed       = 1; % run1 has rng seed 1, run2 has 2.. etc.
-ratio      = 2; % 2 = cone:mRGC ratio = 1:1 (actually 2:2, as one mRGC = ON + OFF cell)
+ratio      = 1; % 2 = cone:mRGC ratio = 1:1 (actually 2:2, as one mRGC = ON + OFF cell)
 eccen      = 1; % equal to 4.5 deg eccen
 
 [PEnergy, PLinear] = linearRGCModel_ClassifyStimTemplate(baseFolder, subFolder, expName, seed, ratio, eccen);
@@ -26,8 +26,8 @@ eccen      = 1; % equal to 4.5 deg eccen
 figure; set(gcf, 'Position',[ 332    39   618   759], 'color','w');
 for ratio = 1:5
     % Load WITH stim template classifier results
-    a=load(fullfile(baseFolder, 'data', expName, 'classification/rgc', subFolder, 'stimTemplate', ...
-        sprintf('classifySVM_rgcResponse_Cones2RGC%d_absorptionrate_%d_defaultnophaseshift_%s.mat',ratio,eccen,subFolder)));
+    a=load(fullfile(baseFolder, 'data', expName, 'classification/rgc',  'stimTemplate',subFolder, ...
+        sprintf('classifySVM_rgcResponse_Cones2RGC%d_absorptions_%d_defaultnophaseshift_%s.mat',ratio,eccen,subFolder)));
     
     % Load WITHOUT stim template classifier results (note, includes all 5
     % ratio's in one file)
@@ -37,27 +37,40 @@ for ratio = 1:5
     contrastLevels.withTemplate = a.expParams.contrastLevels;
     contrastLevels.withoutTemplate = b.expParams.contrastLevels;
     
-    data.withTemplate = a.P_svm;
-    data.withoutTemplate = b.P_svm(ratio,:);
+    performance.templateEnergy = a.PEnergy;
+    performance.templateLinear = a.PLinear;
+    performance.noTemplate = b.P_svm(ratio,:);
     
-    subplot(2,1,1)
-    plot(contrastLevels.withTemplate, data.withTemplate, 'o-'); hold on;
+    logzero = 5.^-4;
+    
+    subplot(3,1,1)
+    plot(contrastLevels.withTemplate, performance.templateEnergy, 'o-', 'lineWidth',2); hold on;
     set(gca, 'XScale', 'log')
     xlabel('Stimulus contrast (fraction)')
     ylabel('Accuracy (%)')
     set(gca, 'TickDir', 'out', 'FontSize',15)
-    plot(10.^-3, data.withTemplate(1), 'o');
-    title({sprintf('SVM Classifier performance for RGC responses WITH template'),'4.5 deg eccen, L-only, no eye movements, no stimulus phase shifts'})
+    plot(logzero, data.withTemplate(1), 'o');
+    title({sprintf('SVM-Energy Classifier performance for RGC responses'),'4.5 deg eccen, L-only, no eye movements, no stimulus phase shifts'})
     
-    subplot(2,1,2)
-    plot(contrastLevels.withoutTemplate, data.withoutTemplate, 'o-'); hold on;
+    subplot(3,1,3)
+    plot(contrastLevels.withTemplate, performance.templateLinear, 'o-', 'lineWidth',2); hold on;
     set(gca, 'XScale', 'log')
     xlabel('Stimulus contrast (fraction)')
     ylabel('Accuracy (%)')
     set(gca, 'TickDir', 'out', 'FontSize',15)
-    plot(10.^-3, data.withoutTemplate(1), 'o')
+    plot(logzero, data.withTemplate(1), 'o');
+    title({sprintf('SVM-Linear Classifier performance for RGC responses'),'4.5 deg eccen, L-only, no eye movements, no stimulus phase shifts'})
     
-    title({sprintf('SVM Classifier performance for RGC responses WITHOUT template'),'4.5 deg eccen, L-only, no eye movements, no stimulus phase shifts'})
+    
+    subplot(3,1,3)
+    plot(contrastLevels.withoutTemplate, data.withoutTemplate, 'o-', 'lineWidth',2); hold on;
+    set(gca, 'XScale', 'log')
+    xlabel('Stimulus contrast (fraction)')
+    ylabel('Accuracy (%)')
+    set(gca, 'TickDir', 'out', 'FontSize',15)
+    plot(logzero, data.withoutTemplate(1), 'o')
+    
+    title({sprintf('SVM-Fourier Classifier performance for RGC responses'),'4.5 deg eccen, L-only, no eye movements, no stimulus phase shifts'})
  
     
 end
