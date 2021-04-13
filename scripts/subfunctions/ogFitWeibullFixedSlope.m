222fcb153532b49e3cd65cb500ae2574cfcb8735
elinekupers <elinekupers@gmail.com>
2021-04-13 11:36:57 -0400

    initial commit of weibull fitting using a provided slope

---
+++ scripts/subfunctions/ogFitWeibullFixedSlope.m
@@ -0,0 +1,40 @@
+function s = ogFitWeibullFixedSlope(threshold, slope, levels, nCorrect, nTotal)
+% Function to fit Weibull function to Oriented Gabor (og) data.
+% There are two parameters to be fitted slope and threshold
+%
+% INPUTS: 
+% vars
+%     vars.b = slope
+%     vars.t = threshold yeilding ~80% correct
+%
+% levels     = independent variable tested on
+% nCorrect   = number of correct trials per level
+% nTotal     = amount of trials per level
+%
+% OUTPUTS:
+% s          = -loglikelihood between data and weibull fit
+
+
+figureOn = 0;
+
+%% compute Weibull
+vars = [slope, threshold];
+
+target = ogWeibull(vars, levels);
+target = target.*.99 + 0.005;
+
+%% compute log likelihood between data and Weibull fit
+
+% size(target)
+% size(nCorrect)
+
+s = -ogLogLikelihood(target, nCorrect', nTotal*ones(1, length(nCorrect)), 'log');
+
+%% visualize
+
+if figureOn
+    figure (1), clf
+    plot(levels, target), hold on
+    plot(levels, nCorrect./nTotal), 
+    drawnow
+end
